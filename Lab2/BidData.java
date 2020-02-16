125099007d89b0cb283203acab9adbd066678b54
fismayilova <40690183+fismayilova@users.noreply.github.com>
2020-02-16 18:11:08 +0400

    Add files via upload

--- Lab2/BidData.java
+++ Lab2/BidData.java
@@ -5,24 +5,17 @@ import java.util.ArrayList;
 public class BidData implements Auctioner {
 	private ArrayList<Bidders> bidders;
 	private String slot_author;
-	int slot_price;
+	private int slot_price;
 	String slot_name;
 
-	public void setMeasurements(int slot_price) {
-		this.slot_price = slot_price;
-		measurementsChanged();
+	public BidData() {
+		bidders = new ArrayList<Bidders>();
 	}
 
-	public void measurementsChanged() {
-		notifyObservers();
-	}
-
-	@Override
 	public void registerObserver(Bidders o) {
 		bidders.add(o);
 	}
 
-	@Override
 	public void removeObserver(Bidders o) {
 		int i = bidders.indexOf(o);
 		if (i >= 0) {
@@ -30,7 +23,6 @@ public class BidData implements Auctioner {
 		}
 	}
 
-	@Override
 	public void notifyObservers() {
 		for (int i = 0; i < bidders.size(); i++) {
 			Bidders observer = (Bidders) bidders.get(i);
@@ -38,15 +30,13 @@ public class BidData implements Auctioner {
 		}
 	}
 
-	public String getSlot_author() {
-		return slot_author;
+	public void measurementsChanged() {
+		notifyObservers();
 	}
 
-	public int getSlot_price() {
-		return slot_price;
+	public void setMeasurements(int slot_price) {
+		this.slot_price = slot_price;
+		measurementsChanged();
 	}
 
-	public int getSlot_name() {
-		return slot_price;
-	}
 }
--- Lab2/CurrentConditionsDisplay.java
+++ Lab2/CurrentConditionsDisplay.java
@@ -1,19 +1,25 @@
 package Lab2;
 
 public class CurrentConditionsDisplay implements Bidders, Display {
+	private String slot_author;
 	private int slot_price;
-	private Auctioner bidData;
+	String slot_name;
+	private Auctioner weatherData;
 
-	public CurrentConditionsDisplay(Auctioner bidData) {
-		this.bidData = bidData;
-		bidData.registerObserver(this);
-	}
-
-	public void update(String slot_author, int slot_price, String slot_name) {
+	public CurrentConditionsDisplay(Auctioner weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
 	}
 
 	public void display() {
 		System.out.println("Current conditions: " + slot_price);
 	}
 
+	@Override
+	public void update(String slot_author, int slot_price, String slot_name) {
+		this.slot_author = slot_author;
+		this.slot_price = slot_price;
+		this.slot_name = slot_name;
+		display();
+	}
 }
