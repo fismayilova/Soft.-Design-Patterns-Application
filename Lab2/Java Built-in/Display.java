8e88e85686fba517191fc215859792959c873a51
fismayilova <40690183+fismayilova@users.noreply.github.com>
2020-02-20 23:03:12 +0400

    Delete Display.java

--- Lab2/Java Built-in/Display.java
+++
@@ -1,9 +0,0 @@
-package JavaObserver;
-
-public interface Display {
-	public void display();
-
-	public static void update(String slot_author, int slot_price, String slot_name) {
-		System.out.println("Author: " + slot_author + " Price: " + slot_price + " Name: " + slot_name);
-	}
-}
